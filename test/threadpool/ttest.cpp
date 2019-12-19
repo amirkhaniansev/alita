e33fc1e10fc4c36e28b7328269ab135241dfb355
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-12-19 16:49:44 +0400

    added threadpool test

---
+++ test/threadpool/ttest.cpp
@@ -0,0 +1,325 @@
+#include "../../include/threadpool.hpp"
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+void* print(void* message)
+{
+    void* result;
+    static int mess_no;
+    std::string msg = "WORK : ";
+    msg += std::to_string(mess_no++);
+    msg += " : ";
+    msg += (char*)message;
+    msg += '\n';
+
+    std::cout << msg;
+
+    for(auto i = 0; i < UINT16_MAX * 1000; i++);
+
+    return result;
+}
+
+void* call(void* in, void* out)
+{
+    std::cout << "CALLBACK CALLED \n";
+    return NULL;
+}
+
+int main(int argc, char** argv)
+{
+    alita::input_t input = (alita::input_t)"message";
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().enqueue_work(print, input, call);
+    alita::threadpool::instance().state();
+
+    std::this_thread::sleep_for(std::chrono::seconds(15));
+
+    alita::threadpool::instance().state();
+
+    return 0;
+}
