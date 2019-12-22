4b827a79530d7428d319433cd0a450f5e60014e8
Sevak Amirkhanian <amirkhanyan.sevak@gmail.com>
2019-12-22 23:04:31 +0400

    merged PR Build Script (#20)
    * added build script
    
    * truncated HTML test file

---
+++ build/build.sh
@@ -0,0 +1,8 @@
+cd ./https/build && make -B && cd ../../
+cd ./lqueue/build && make -B && cd ../../
+cd ./mqueue/build && make -B && cd ../../
+cd ./parser/build && make -B && cd ../../
+cd ./threadpool/build && make -B && cd ../../
+cd ./crawler/build && make -B && cd ../../
+cd ./pocs/build && make -B && cd ../../
+cd ./test/build && make -B && cd ../../
